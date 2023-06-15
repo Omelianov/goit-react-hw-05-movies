720b1303dc5acd203e3f0b66b18edf5d19eabda0
Omelianov Nicolai <omelianov.n@gmail.com>
2023-06-15 10:13:01 +0300

    Update Container.styled.js

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -4,7 +4,7 @@ export const ContainerWrapper = styled.div`
   width: 300px;
   margin: 0 auto;
   padding: ${props =>
-    props.isPadding ? '0 10px 100px 10px' : '20px 10px 20px 10px'};
+    props.isPadding ? '0 10px 100px 10px' : '20px'};
 
   @media (min-width: 768px) {
     width: 748px;
@@ -12,5 +12,8 @@ export const ContainerWrapper = styled.div`
 
   @media (min-width: 1440px) {
     width: 1420px;
+
   }
+
+  
 `;
